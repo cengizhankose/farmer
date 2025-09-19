719455b6c94b467009b69627787d24d879b5c24c
Efe Akkurt <efeakkurtofficial@gmail.com>
2025-09-19 21:31:40 +0300

    implemented Who Are We? section

---
+++ apps/web/components/TeamWhyShowcase/useScrollStage.ts
@@ -0,0 +1,11 @@
+import { RefObject } from 'react'
+import { useInView } from 'framer-motion'
+
+export function useScrollStage(ref: RefObject<Element>) {
+  const isInView = useInView(ref, {
+    amount: 0.3,
+    once: true,
+  })
+
+  return { play: isInView }
+}
